d599645acab28a18aaa002e2790c5fb5debccda4
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-18 18:28:46 -0300

    refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
+import MainLayout from './layouts/MainLayout';
 import Workspace from './components/Workspace';
 
 function App() {
   return (
-    <div className="flex h-screen bg-slate-900 overflow-hidden font-sans text-slate-100">
-      <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Navbar />
-        {/* Main Content Area */}
-        <div className="flex-1 overflow-auto relative z-0">
-          <Routes>
-            <Route path="/" element={<Workspace />} />
-            {/* Add more routes here as needed (Tasks, Calendar, Settings) */}
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Workspace />} />
+      </Route>
+    </Routes>
   );
 }
 
