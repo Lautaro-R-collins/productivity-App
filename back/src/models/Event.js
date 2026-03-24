97f9154541c659c7e0d4ddf30dd9b0296ec48e7c
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-24 17:09:37 -0300

    add schema event calendar (back)

---
+++ back/src/models/Event.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  start: { type: Date, required: true },
+  end: { type: Date, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
