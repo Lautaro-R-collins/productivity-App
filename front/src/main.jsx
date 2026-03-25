a956e0049764f305d92832ad22d910af04a9214e
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-25 15:12:55 -0300

    add provider - main front

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,13 +4,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PomodoroProvider } from './context/PomodoroContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <PomodoroProvider>
-        <App />
-      </PomodoroProvider>
+      <AuthProvider>
+        <PomodoroProvider>
+          <App />
+        </PomodoroProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
