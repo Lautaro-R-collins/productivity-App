aa07351b477e299ec705a0fd7030cbaf6eb0f2f2
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-25 15:08:54 -0300

    controllers event back

--- back/src/controllers/eventController.js
+++ back/src/controllers/eventController.js
@@ -2,7 +2,7 @@ const Event = require('../models/Event');
 
 exports.getEvents = async (req, res) => {
   try {
-    const events = await Event.find();
+    const events = await Event.find({ user: req.user.id });
     res.json(events);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -12,7 +12,7 @@ exports.getEvents = async (req, res) => {
 exports.createEvent = async (req, res) => {
   try {
     const { title, start, end } = req.body;
-    const newEvent = new Event({ title, start, end });
+    const newEvent = new Event({ title, start, end, user: req.user.id });
     await newEvent.save();
     res.status(201).json(newEvent);
   } catch (error) {
@@ -22,7 +22,7 @@ exports.createEvent = async (req, res) => {
 
 exports.updateEvent = async (req, res) => {
   try {
-    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedEvent = await Event.findOneAndUpdate({ _id: req.params.id, user: req.user.id }, req.body, { new: true });
     res.json(updatedEvent);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -31,7 +31,7 @@ exports.updateEvent = async (req, res) => {
 
 exports.deleteEvent = async (req, res) => {
   try {
-    await Event.findByIdAndDelete(req.params.id);
+    await Event.findOneAndDelete({ _id: req.params.id, user: req.user.id });
     res.json({ message: 'Event successfully removed' });
   } catch (error) {
     res.status(500).json({ message: error.message });
