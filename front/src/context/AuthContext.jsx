533b6c0a26ffa60bc663f1f1a613ea00344c185b
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-25 15:13:26 -0300

    context auth

---
+++ front/src/context/AuthContext.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { createContext, useState, useEffect } from 'react';
+import api from '../services/api';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [loading, setLoading] = useState(true);
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (token) {
+        try {
+          const res = await api.get('/auth/me');
+          setUser(res.data);
+        } catch (error) {
+          console.error('Context Auth token expired or invalid', error);
+          logout();
+        }
+      }
+      setLoading(false);
+    };
+    fetchUser();
+  }, [token]);
+
+  const login = async (email, password) => {
+    const res = await api.post('/auth/login', { email, password });
+    localStorage.setItem('token', res.data.token);
+    setToken(res.data.token);
+    setUser({ _id: res.data._id, username: res.data.username, email: res.data.email });
+  };
+
+  const register = async (username, email, password) => {
+    const res = await api.post('/auth/register', { username, email, password });
+    localStorage.setItem('token', res.data.token);
+    setToken(res.data.token);
+    setUser({ _id: res.data._id, username: res.data.username, email: res.data.email });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, loading, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
