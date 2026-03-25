0e023d9d0c2d5195e022f9a2003995b53e5119b0
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-25 15:11:17 -0300

    protect routers

--- back/src/routes/events.js
+++ back/src/routes/events.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.use(protect);
 
 router.get('/', eventController.getEvents);
 router.post('/', eventController.createEvent);
