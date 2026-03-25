b39642f3ed049fc927e1a59bdcf01e67a8a97359
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-03-25 15:10:03 -0300

    add user srchema

--- back/src/models/Event.js
+++ back/src/models/Event.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   start: { type: Date, required: true },
   end: { type: Date, required: true },
